ca34a6f37adba6852876b6c0f595681c216cd5ba
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2025-01-12 15:54:28 -0500

    fixed up prelim.sh for command installation

--- boot/bash/prelim.sh
+++ boot/bash/prelim.sh
@@ -1,3 +1,31 @@
-#!bin/bash
+#!/bin/bash
 
-echo "Test on Ubuntu Linux"
+# Define colors
+
+
+GREEN='\033[32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Loop through the tools
+for tool in asdf nmap ping tcpdump dig arp ifconfig route networksetup; do
+    if ! command -v $tool &> /dev/null; then
+        echo -e "${RED}$tool is not installed${NC}"
+        echo -e "Trying to install required libraries"
+        if brew search "$tool" | grep -q "^$tool$"; then
+            echo "Found $tool in Homebrew. Installing..."
+            brew install "$tool"
+            if [[ $? -eq 0 ]]; then
+                echo "$tool has been successfully installed."
+            else
+                echo "Failed to install $tool."
+            fi
+        
+        else
+            echo "$tool is not available in Homebrew."
+        fi
+        
+    else
+        echo -e "${GREEN}Checking: $tool ...${NC}"
+    fi
+done
